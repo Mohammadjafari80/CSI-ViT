eacbab0fdd9f9740deeda3e4406e116c17004962
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-10-11 00:50:27 +0330

    apply GELU & Dropout

--- models/resnet.py
+++ models/resnet.py
@@ -15,18 +15,22 @@ from models.transform_layers import NormalizeLayer
 from torch.nn.utils import spectral_norm
 from torchvision import models
 
+import torch.nn as nn
+import torch.nn.functional as F
+
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.0):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(in_planes, planes, stride)
-        self.conv2 = conv3x3(planes, planes)
         self.bn1 = nn.BatchNorm2d(planes)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -37,23 +41,24 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.gelu(self.bn1(self.conv1(x)))
+        out = self.dropout(out)
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.gelu(out)
         return out
 
-
 class PreActBlock(nn.Module):
-    '''Pre-activation version of the BasicBlock.'''
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.0):
         super(PreActBlock, self).__init__()
-        self.conv1 = conv3x3(in_planes, planes, stride)
-        self.conv2 = conv3x3(planes, planes)
         self.bn1 = nn.BatchNorm2d(in_planes)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.conv1 = conv3x3(in_planes, planes, stride)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -62,25 +67,28 @@ class PreActBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
+        out = self.gelu(self.bn1(x))
+        out = self.dropout(out)
         shortcut = self.shortcut(out)
         out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.dropout(self.gelu(self.bn2(out)))
+        out = self.conv2(out)
         out += shortcut
         return out
-
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.0):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -90,26 +98,28 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.gelu(self.bn1(self.conv1(x)))
+        out = self.dropout(out)
+        out = self.gelu(self.bn2(self.conv2(out)))
+        out = self.dropout(out)
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.gelu(out)
         return out
 
-
 class PreActBottleneck(nn.Module):
-    '''Pre-activation version of the original Bottleneck module.'''
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.0):
         super(PreActBottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(in_planes)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -118,17 +128,21 @@ class PreActBottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
+        out = self.gelu(self.bn1(x))
+        out = self.dropout(out)
         shortcut = self.shortcut(out)
         out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out = self.conv3(F.relu(self.bn3(out)))
+        out = self.dropout(self.gelu(self.bn2(out)))
+        out = self.conv2(out)
+        out = self.dropout(self.gelu(self.bn3(out)))
+        out = self.conv3(out)
         out += shortcut
         return out
 
 
+
 class ResNet(BaseModel):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, dropout_rate=0.0):
         last_dim = 512 * block.expansion
         super(ResNet, self).__init__(last_dim, num_classes)
 
@@ -139,17 +153,19 @@ class ResNet(BaseModel):
 
         self.conv1 = conv3x3(3, 64)
         self.bn1 = nn.BatchNorm2d(64)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(p=dropout_rate)
 
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, dropout_rate=dropout_rate)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, dropout_rate=dropout_rate)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, dropout_rate=dropout_rate)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, dropout_rate=dropout_rate)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, dropout_rate):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, dropout_rate))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -157,9 +173,8 @@ class ResNet(BaseModel):
         out_list = []
 
         out = self.normalize(x)
-        out = self.conv1(out)
-        out = self.bn1(out)
-        out = F.relu(out)
+        out = self.gelu(self.bn1(self.conv1(out)))
+        out = self.dropout(out)
         out_list.append(out)
 
         out = self.layer1(out)
@@ -178,6 +193,7 @@ class ResNet(BaseModel):
             return out, out_list
         else:
             return out
+
 
 class Pretrain_ResNet(BaseModel):
     def __init__(self, block, num_blocks, num_classes=10):
